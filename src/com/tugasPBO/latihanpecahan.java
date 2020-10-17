20908feaae70dcfd298b68e93f28079d4e7f0de5
unknown <argya.programmer@gmail.com>
2020-10-17 14:28:14 +0700

    Initial Commit

---
+++ src/com/tugasPBO/latihanpecahan.java
@@ -0,0 +1,15 @@
+//latihan 8 PBO
+// Nama       : Argya Aulia Fauzandika
+// NIM        : 10119035
+// Kelas      : IF-1
+// Deskripsi  : menampilkan bilangan pecahan float dan double
+package com.tugasPBO;
+
+public class latihanpecahan {
+    public static void main(String[] args){
+        float f = 1234567890.123456789F;
+        double d = 1_234_567_890.123456789;
+        System.out.println("Float : "+ f);
+        System.out.println("Double : "+ d);
+    }
+}
